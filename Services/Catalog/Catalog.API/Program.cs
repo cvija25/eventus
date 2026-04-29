7e1f3e74f7d4bc9d3abf435df8d2cac50049cc25
Đorđe Marić <djordjegmaric@gmail.com>
2026-04-30 01:00:25 +0200

    feat: catalog api skeleton

---
+++ Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
