ed5e75feeb4c3e581a135f7947e8759dacf91485
Đorđe Marić <djordjegmaric@gmail.com>
2026-06-01 19:59:17 +0200

    wip

--- Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,15 +1,42 @@
+using Catalog.Common.DTOs;
+using Catalog.Common.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Controllers;
 
 [ApiController]
-[Route("/api/v1/catalog")]
+[Route("/api/v1/catalog/events")]
 public class CatalogController : ControllerBase
 {
-    [HttpGet]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public ActionResult<string> GetGreeting()
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(EventDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<EventDto>> GetEvent(int id)
     {
-        return Ok("Hello World, from Catalog!");
+        var ev = await _eventRepository.GetEventByIdAsync(id);
+        return ev is null ? NotFound() : Ok(ev);
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<EventDto>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<EventDto>>> GetAllEvents()
+    {
+        var events = await _eventRepository.GetEventsAsync();
+        return Ok(events);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<EventDto>> CreateEvent([FromBody] CreateEventDto dto)
+    {
+        
+    }
+
+    private readonly IEventRepository _eventRepository;
+
+    public CatalogController(IEventRepository eventRepository)
+    {
+        _eventRepository = eventRepository;
     }
 }
